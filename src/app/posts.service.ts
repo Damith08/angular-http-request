b4258fedcaaa055e5e11e5dc33c65d819a070f5b
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-07-28 17:47:54 +0530

    feat: sending a delete request
    Section 18 - Sending a DELETE Request

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -36,4 +36,10 @@ export class PostsService {
         })
       );
   }
+
+  deletePosts() {
+    return this.http.delete(
+      'https://ng-complete-guide-47578-default-rtdb.firebaseio.com/posts.json'
+    );
+  }
 }
