9bb73a6c63a8d6f8b5823735cc4035b42055e408
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-07-28 15:24:57 +0530

    feat: use types with the http request
    Section 18 - Using Types with the HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +17,11 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
       .post(
-        'https://ng-complete-guide-47578-default-rtdb.firebaseio.com/post.json',
+        'https://ng-complete-guide-47578-default-rtdb.firebaseio.com/posts.json',
         postData
       )
       .subscribe((responseData) => {
@@ -38,10 +40,22 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http
-      .get(
-        'https://ng-complete-guide-47578-default-rtdb.firebaseio.com/post.json'
+      .get<{ [key: string]: Post }>(
+        'https://ng-complete-guide-47578-default-rtdb.firebaseio.com/posts.json'
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
       )
       .subscribe((posts) => {
+        // ...
         console.log(posts);
       });
   }
