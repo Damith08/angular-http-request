3a984791f3d88e0e48161616044aa57a0c1df048
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-07-28 15:50:14 +0530

    feat: show a loading indicator
    Section 18 - Showing a Loading Indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -39,6 +40,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>(
         'https://ng-complete-guide-47578-default-rtdb.firebaseio.com/posts.json'
@@ -55,7 +57,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe((posts) => {
-        // ...
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
