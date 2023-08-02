155f2150009e5d854437d2535666d8036c3f3028
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-08-02 11:56:29 +0530

    feat: observing different types of response
    Section 18 - Observing Different Types of Responses

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,13 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpEventType,
+  HttpHeaders,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
-import { map, catchError } from 'rxjs/operators';
-import { Subject, throwError } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+import { Subject, pipe, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -15,7 +20,10 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         'https://ng-complete-guide-47578-default-rtdb.firebaseio.com/posts.json',
-        postData
+        postData,
+        {
+          observe: 'response',
+        }
       )
       .subscribe(
         (responseData) => {
@@ -57,8 +65,23 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete(
-      'https://ng-complete-guide-47578-default-rtdb.firebaseio.com/posts.json'
-    );
+    return this.http
+      .delete(
+        'https://ng-complete-guide-47578-default-rtdb.firebaseio.com/posts.json',
+        {
+          observe: 'events',
+        }
+      )
+      .pipe(
+        tap((event) => {
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+            //...
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
